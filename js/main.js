51b01de254bbb30695725009cc87b0fd7e4eb997
prostoG <a.opk12@yandex.ru>
2022-07-15 14:35:16 +0300

    add backend

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// (function () {
+
+//     async function getAPI () {
+//         let response = await fetch('http://localhost:3000/api/clients')
+//         let data = await response.json()
+//         console.log(data)
+//         return data
+//     }
+// getAPI()
+
+// })()
